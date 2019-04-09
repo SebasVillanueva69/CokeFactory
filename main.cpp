41a309d611be6e92569a9198938528cca92df04e
madri308 <ememadrigal.em@gmail.com>
2019-04-09 00:27:51 -0600

    Hice un pichazo
    Mae literal solo falta la interfaz y las lista doblemente enlazadas

--- main.cpp
+++ main.cpp
@@ -1,11 +1,51 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include "structs.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    Machine* cleaner = new Machine("clean",3);
+    Machine* filler1 = new Machine("fill",4);
+    Machine* filler2 = new Machine("fill",4);
+    Machine* capper = new Machine("cap",1);
+
+    Queue* band1 = new Queue(100);
+    Queue* bandP2 = new Queue(50);
+    Queue* bandP3 = new Queue(80);
+    Queue* band4 = new Queue(10);
+    Queue* band5 = new Queue(120);
+
+    Person* setter = new Person("colocador",5);
+    Person* checker = new Person("inspector",10);
+
+    Bottle* botella = new Bottle("PITUFA");
+    Bottle* Sbottle = setter->startWork(botella);
+    if(!band1->isFull()){
+        Node* node = new Node(Sbottle);
+        band1->enqueue(node);
+    }
+    Bottle* Cbottle = cleaner->startMachine(band1->dequeue()->data);
+    if(!bandP2->isFull()){
+        Node* node = new Node(Cbottle);
+        bandP2->enqueue(node);
+    }
+    Bottle* Fbottle = filler1->startMachine(bandP2->dequeue()->data);
+    if(!band4->isFull()){
+        Node* node = new Node(Fbottle);
+        band4->enqueue(node);
+    }
+    Bottle* CaBottle = capper->startMachine(band4->dequeue()->data);
+    if(!band5->isFull()){
+        Node* node = new Node(CaBottle);
+        band5->enqueue(node);
+    }
+    checker->startWork(band5->dequeue()->data);
+
+
 
     return a.exec();
 }
--- structs.h
+++ structs.h
@@ -3,17 +3,20 @@
 #include<stdlib.h>
 #include <string>
 #include <vector>
+#include <thread>
+#include <chrono>
+
 using namespace std;
 
 struct Bottle
 {
     string type; //PITUFA(250ml), 1/2LITROS(500ml), 2LITROS(2000ml)
 
-    bool isClean; //Pasa por limpia botellas
-    bool isEmpty; //Llenan
-    bool isCap; //Se tapan
-    bool isCheck; //Se inspeccionan
-    bool isReady; //Se encajan
+    bool isClean = false; //Pasa por limpia botellas
+    bool isEmpty = true; //Llenan
+    bool isCap = false; //Se tapan
+    bool isCheck = false; //Se inspeccionan
+    bool isReady = false; //Se encajan
 
     Bottle(string type) {
         this->type = type;
@@ -25,9 +28,12 @@ struct Person
     string type; //Carga o inspeccionan
     int status = 0; //0 = OFF, 1 = ON, 2 = PAUSE
     int speed;
-    vector<int> rejectPercentage = {5,20,10}; //0 = pitufas, 1 = 1/2litro, 2 = 2litros
-    vector<int> quantityPercentage = {50,25,25}; //0 = pitufas, 1 = 1/2litro, 2 = 2litros
-    Person(int speed) {
+    //INSPECTOR: PORCENTAJE DE BOTELLAS QUE SERAN RECHAZADAS.
+    //vector<int> rejectPercentage = {5,20,10}; //0 = pitufas, 1 = 1/2litro, 2 = 2litros
+    //CARGADOR: PORCENTAJE DE BOTELLAS QUE CARGARA.
+    //vector<int> quantityPercentage = {50,25,25}; //0 = pitufas, 1 = 1/2litro, 2 = 2litros
+    Person(string type, int speed) {
+        this->type = type;
         this->speed = speed;
     }
     void changeSpeed(int newSpeed){
@@ -36,15 +42,26 @@ struct Person
     void changeStatus(int newStatus){
         this->status = newStatus;
     }
+    Bottle* startWork(Bottle* bottle){
+        this->status = 1;
+        if(this->type == "inspector"){ //FALTA LO DE LA PROBABILIDAD DE DESECHAR
+            bottle->isCheck = true;
+        }
+        for (int i=this->speed; i>0; --i) {
+            this_thread::sleep_for (std::chrono::seconds(1));
+            //Actualizar segundos que se muestran.
+        }
+        return bottle;
+    }
 };
 
 struct Machine
 {
-    string type;
+    string type;//clean, fill, cap,
     std::vector<int> counters = {0,0,0}; //0 = pitufas, 1 = 1/2litro, 2 = 2litros
     int speed;
     int status = 0; //0 = OFF, 1 = ON, 2 = PAUSE
-    Machine(int speed, string type) {
+    Machine(string type, int speed) {
         this->speed = speed;
         this->type = type;
     }
@@ -54,13 +71,28 @@ struct Machine
     void changeStatus(int newStatus){
         this->status = newStatus;
     }
+    Bottle* startMachine(Bottle* bottle){
+        this->status = 1;
+        if(type == "clean"){
+            bottle->isClean = true;
+        }else if(type == "fill"){
+            bottle->isEmpty = false;
+        }else {
+            bottle->isCap = true;
+        }
+        for (int i=this->speed; i>0; --i) {
+            this_thread::sleep_for (std::chrono::seconds(1));
+            //Actualizar segundos que se muestran.
+        }
+        return bottle;
+    }
 };
 
-struct trucks
+struct Trucks
 {
     string type;
     std::vector<int> capacity = {0,0,0}; //0 = pitufas, 1 = 1/2litro, 2 = 2litros
-    trucks(string type) {
+    Trucks(string type) {
         this->type = type;
         if(type == "pickup"){
             capacity.at(0) = 200;
@@ -76,6 +108,53 @@ struct trucks
             capacity.at(2) = 200;
         }
     }
+};
+
+//--------------BIG DATA STRUCTS
+
+struct Node
+{
+    Bottle *data = nullptr;
+    Node *next = nullptr;
+    Node(Bottle* data) {
+        this->data = data;
+    }
+};
+
+struct Queue
+{
+    int count;
+    int limit;
+    Node* first = nullptr;
+    Queue(int limit) {
+        this->limit = limit;
+    }
+    void changelimit(int newLimit){
+        this->limit = newLimit;
+    }
+    Node* dequeue(){
+        Node* toReturn = first;
+        first->next = first;
+        return toReturn;
+    }
+    void enqueue(Node* newBottle){
+        count++;
+        if(first == nullptr){
+            this->first = newBottle;
+        }else{
+            Node* temp = first;
+            while(first->next != nullptr){
+                temp = temp->next;
+            }
+            temp->next = newBottle;
+        }
+    }
+    bool isFull(){
+        if(count == limit){
+            return true;
+        }return false;
+    }
+
 };
 
 #endif // STRUCTS_H
