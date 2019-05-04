7e744df9f775cc833cf3053500004c9d915b5d0b
madri308 <ememadrigal.em@gmail.com>
2019-05-03 22:40:10 -0600

    Ventana primera integrada

--- firstwindow.cpp
+++ firstwindow.cpp
@@ -1,20 +1,36 @@
 #include "firstwindow.h"
 #include "mainwindow.h"
 #include "ui_firstwindow.h"
+#include "iostream"
+#include "QStringListModel"
 
+using namespace std;
 firstWindow::firstWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::firstWindow)
 {
     ui->setupUi(this);
+    this->setCentralWidget(ui->verticalWidget);
+    ui->verticalWidget->setFixedSize(500,270);
+    this->adjustSize();
 
     connect(ui->start, SIGNAL (released()),this, SLOT (start()));
+    connect(ui->add, SIGNAL (released()),this, SLOT (add()));
+
 }
 void firstWindow::start(){
     MainWindow *w = new MainWindow();
     w->showMaximized();
     this->hide();
 }
+void firstWindow::add(){
+    QString type = ui->tipo->toPlainText() +" "+ ui->proba->toPlainText()+"%";
+    ui->listWidget->addItem(type);
+    ui->listWidget->update();
+    ui->tipo->clear();
+    ui->proba->clear();
+}
+
 
 firstWindow::~firstWindow()
 {
